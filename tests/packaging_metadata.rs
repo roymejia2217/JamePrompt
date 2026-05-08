36405a0e7f8482fc09674bed449b1ef73907e14a
Roy Mejia <roymejia2217@gmail.com>
2026-05-07 22:57:59 -0500

    fix(release): use authenticated downloads for AppImage tools

--- tests/packaging_metadata.rs
+++ tests/packaging_metadata.rs
@@ -212,6 +212,8 @@ fn release_workflow_includes_windows_artifacts_in_the_shared_release_pipeline() 
             "cargo metadata --format-version 1 --no-deps",
             "Where-Object { $_.kind -contains \"bin\" }",
             "Copy-Item $source \"$portableRoot/$binaryName.exe\"",
+            "gh release download --repo linuxdeploy/linuxdeploy",
+            "gh release download --repo AppImage/appimagetool",
         ],
     );
     assert!(
